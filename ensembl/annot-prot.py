419741caed9868a3a025e762e93ce1bea1553940
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2013-07-31 22:09:49 -0500

    A script to put gene name on ensenbl prot seq.

---
+++ ensembl/annot-prot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import os
+import sys
+
+filename_names = sys.argv[1]
+filename_prot = sys.argv[2]
+
+names = dict()
+
+f_names = open(filename_names,'r')
+headers = f_names.readline().strip().split("\t")
+idx_gene_id = headers.index('Ensembl Gene ID')
+idx_chr_name = headers.index('Chromosome Name')
+idx_gene_name = headers.index('Associated Gene Name')
+
+for line in f_names:
+    tokens = line.split("\t")
+    tmp_gene_id = tokens[idx_gene_id]
+    tmp_gene_name = tokens[idx_gene_name].strip().replace(':','_').replace(' ','')
+    tmp_chr_name = tokens[idx_chr_name]
+
+    #if( len(tmp_chr_name) > 2 ):
+    #    continue
+    if( tmp_gene_name == '' ):
+        names[tmp_gene_id] = 'NA'
+    else:
+        names[tmp_gene_id] = tmp_gene_name.strip()
+f_names.close()
+
+f_prot = open(filename_prot,'r')
+for line in f_prot:
+    if( line.startswith('>') ):
+        tokens = line.strip().lstrip('>').split('|')
+        gene_id = tokens[-1]
+        
+        if( names.has_key(gene_id) ):
+            print ">%s|%s"%(names[gene_id],'|'.join(tokens))
+        else:
+            print ">NA|%s"%('|'.join(tokens))
+    else:
+        print line.strip()
+f_prot.close()
