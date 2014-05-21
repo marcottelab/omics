4ddae82792debbc33864fdbe52e5953e8d123a7b
Taejoon Kwon <linusben@gmail.com>
2014-05-21 08:54:06 -0500

    Script to download uniprot files.

---
+++ uniprot/wget.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget http://www.uniprot.org/docs/speclist
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
