04e7bf0af90a49cfcd1e880f306e2a772a269a70
Taejoon Kwon <linusben@gmail.com>
2014-05-22 10:34:52 -0500

    Download reldate and README (uniprot)

--- uniprot/wget.sh
+++ uniprot/wget.sh
@@ -2,3 +2,5 @@
 wget http://www.uniprot.org/docs/speclist
 wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
 wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/reldate.txt
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/README
